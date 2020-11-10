c17d8af9717d5e9332a96f2c0719a52c486455dd
Steven <clai86@my.bcit.ca>
2020-11-09 21:14:38 -0800

    Collapsable Navbar Component Created #7

---
+++ movie-app/src/components/Navigation.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+
+export default class Navigation extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar bg="dark" expand="lg" variant="dark">
+                    <Navbar.Brand href="#home">Movie App</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#">Home</Nav.Link>
+                            <Nav.Link href="#">Move</Nav.Link>
+                            <Nav.Link href="#">TV Shows</Nav.Link>
+                            <Nav.Link href="#">Latest</Nav.Link>
+                        </Nav>
+                        <Form inline>
+                            <FormControl
+                                type="text"
+                                placeholder="Search"
+                                className="mr-sm-2"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
