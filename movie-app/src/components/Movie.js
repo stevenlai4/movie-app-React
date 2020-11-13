c6fe1690b2083e1453a87d7cf8c810b8a6d79c5e
Steven <clai86@my.bcit.ca>
2020-11-13 15:38:19 -0800

    Add movie items through API #9

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -1,53 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MovieItem from './MovieItem';
 
-export default class Movie extends Component {
-    render() {
+function Movie({ movies }) {
+    // Create MovieItem component function
+    const createMovieItem = (movie) => {
         return (
-            <div class="container mt-5">
-                <div className="row row-cols-4">
-                    <MovieItem
-                        img="movie-img/aquaman.jpeg"
-                        name="Aquaman"
-                        date="12/21/2019"
-                    />
-                    <MovieItem
-                        img="movie-img/avengers-end-game.jpg"
-                        name="Avengers: Endgame"
-                        date="04/26/2019"
-                    />
-                    <MovieItem
-                        img="movie-img/black-panther.jpg"
-                        name="Black Panther"
-                        date="02/16/2018"
-                    />
-                    <MovieItem
-                        img="movie-img/fast-and-furious-8.jpeg"
-                        name="Fast And Furious 8"
-                        date="04/13/2019"
-                    />
-                    <MovieItem
-                        img="movie-img/joker.jpg"
-                        name="Joker"
-                        date="10/30/2019"
-                    />
-                    <MovieItem
-                        img="movie-img/mulan.jpg"
-                        name="Mulan"
-                        date="09/04/2020"
-                    />
-                    <MovieItem
-                        img="movie-img/skyscraper.jpg"
-                        name="Skyscraper"
-                        date="07/13/2020"
-                    />
-                    <MovieItem
-                        img="movie-img/spiderman-far-from-home.jpg"
-                        name="Spiderman: Far From Home"
-                        date="08/21/2019"
-                    />
-                </div>
-            </div>
+            <MovieItem
+                key={movie.id}
+                img={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                name={movie.original_title}
+                date={movie.release_date}
+            />
         );
-    }
+    };
+
+    // Generate movie items
+    return (
+        <div className="container mt-5">
+            <div className="row row-cols-lg-4 row-cols-md-3 row-cols-1">
+                {movies.map(createMovieItem)}
+            </div>
+        </div>
+    );
 }
+
+export default Movie;
--- movie-app/src/components/MovieItem.js
+++ movie-app/src/components/MovieItem.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import '../styles/MovieItem.scss';
 
 export default class MovieItem extends Component {
     render() {
         return (
             <div className="col mb-4">
-                <div className="card">
+                <div className="card movie-item">
                     <img
                         src={this.props.img}
                         className="card-img-top"
                         alt={this.props.name}
                     />
                     <div className="card-body">
-                        <h5 className="card-title">{this.props.name}</h5>
+                        <h5 className="card-title movie-title">
+                            {this.props.name}
+                        </h5>
                         <p className="card-text">
                             Release Date: {this.props.date}
                         </p>
