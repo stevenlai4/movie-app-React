db9bf6c99ed8d0c2eaf527c3bc3bfb42a41fecfb
Steven <clai86@my.bcit.ca>
2020-11-13 17:53:17 -0800

    Populate API data

--- movie-app/src/components/App.js
+++ movie-app/src/components/App.js
@@ -1,16 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import '../styles/App.scss';
 import Banner from './Banner';
 import Movie from './Movie';
 import Navigation from './Navigation';
 
 function App() {
-    return (
-        <div className="App">
-            <Banner />
-            <Navigation />
-            <Movie />
-        </div>
-    );
+    const POPULAR_MOVIE_API =
+        'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5539be3a42f8533e3aa1c749481b4c63&page=1';
+
+    // State
+    const [movies, setMovies] = useState({
+        movies: [],
+        isLoading: true,
+    });
+
+    // ComponentDidMount() - Request Popular Movie API
+    useEffect(() => {
+        fetch(POPULAR_MOVIE_API)
+            .then((res) => res.json())
+            .then((json) => {
+                setMovies({
+                    movies: json.results,
+                    isLoading: false,
+                });
+            });
+    }, []);
+
+    if (movies.isLoading) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className="App">
+                <Banner movies={movies.movies} />
+                <Navigation />
+                <Movie movies={movies.movies} />
+            </div>
+        );
+    }
 }
 
 export default App;
