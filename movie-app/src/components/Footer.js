98a340871f3644680f468f046a345351a96649dc
Steven <clai86@my.bcit.ca>
2020-11-16 14:52:22 -0800

    Add footer component and style

--- movie-app/src/components/Footer.js
+++ movie-app/src/components/Footer.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { GrFacebook } from 'react-icons/gr';
+import {
+    GrFacebook,
+    GrInstagram,
+    GrTwitter,
+    GrPinterest,
+} from 'react-icons/gr';
 import '../styles/global/footer.scss';
 
 export default function Footer() {
     return (
         <footer className="bg-dark">
+            <p class="footer-title">Movie App</p>
             <ul>
                 <li>
                     <a href="#">
                         <GrFacebook />
                     </a>
                 </li>
+                <li>
+                    <a href="#">
+                        <GrInstagram />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <GrTwitter />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <GrPinterest />
+                    </a>
+                </li>
             </ul>
+
+            <p class="copyright">Copyright &#169; 2020 Chao Chun Lai</p>
         </footer>
     );
 }
